b506a7cbac783971c1cdf500d9b59471d5d1bd17
TomRivaud <temtonik@gmail.com>
2018-10-02 19:24:52 +0200

    Scene Accueil + Scene Jeu
    - Scène Accueil avec liaison vers la scène Jeu (bouton Play) et bouton Quit (quitte le jeu).
    - Scène Jeu avec menu pause fonctionnel, bouton Resume qui reprend le jeu et bouton Quit qui mène à la scène Accueil.
    
    L'UI des boutons et le background peut être ajouté à ces scènes

---
+++ Projet_Mobile_F2P/Assets/Scripts/UI/AccueilScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AccueilScript : MonoBehaviour {
+
+    public string SceneToLoadPlay;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayButton ()
+    {
+        SceneManager.LoadScene(SceneToLoadPlay);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
---
+++ Projet_Mobile_F2P/Assets/Scripts/UI/Pause.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Pause : MonoBehaviour {
+
+    public bool PauseActive = false;
+    public GameObject PauseMenuUI;
+
+    public string SceneToLoadQuit;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (PauseActive)
+            {
+                ResumeButton();
+            }
+            else
+            {
+                MenuPause();
+            }
+            
+        }
+	}
+
+    public void MenuPause()
+    {
+        PauseActive = true;
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeButton()
+    {
+        PauseActive = false;
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void QuitButton()
+    {
+        SceneManager.LoadScene(SceneToLoadQuit);
+    }
+}
