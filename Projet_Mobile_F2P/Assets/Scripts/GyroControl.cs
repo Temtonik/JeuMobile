dcf44322d2f40ffe12b1048ec0721bd76568ef2e
TomRivaud <temtonik@gmail.com>
2018-10-02 14:52:31 +0200

    Gyrocontrol

---
+++ Projet_Mobile_F2P/Assets/Scripts/GyroControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroControl : MonoBehaviour {
+
+    public GameObject player;
+
+    void Start()
+    {
+        Input.gyro.enabled = true;
+    }
+
+    void Update()
+    {
+        player.transform.Rotate(0, -Input.gyro.rotationRateUnbiased.y, 0);
+
+    }
+}
