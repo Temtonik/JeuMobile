f7f1e7af573745735bd349618462dd3ab6a62c16
TomRivaud <temtonik@gmail.com>
2018-10-09 15:24:36 +0200

    TriggerEnd

---
+++ Projet_Mobile_F2P/Assets/Scripts/UI/FinishLevel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLevel : MonoBehaviour {
+
+    public string nextLevel;
+
+    public GameObject victoryCanvas;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            victoryCanvas.SetActive(true);
+        }
+    }
+
+    public void GoToNextLevel()
+    {
+        SceneManager.LoadScene(nextLevel);
+    }
+}
