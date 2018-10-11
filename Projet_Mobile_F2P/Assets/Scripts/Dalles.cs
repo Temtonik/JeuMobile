61a2493eaf892a72c7474b5283591c0236aa264f
TomRivaud <temtonik@gmail.com>
2018-10-11 15:00:08 +0200

    Dalles

---
+++ Projet_Mobile_F2P/Assets/Scripts/Dalles.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dalles : MonoBehaviour {
+
+    public SwapMove my_SwapMove;
+
+    public int activatedPress;
+    public int numberPressInLevel;
+
+    public GameObject triggerPress;
+    public GameObject feedbackPress;
+    public GameObject door;
+    public GameObject triggerRaycast;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && my_SwapMove.is_Standing == true)
+        {
+            activatedPress += 1;
+            triggerPress.SetActive(false);
+            feedbackPress.SetActive(true);
+            
+            if (activatedPress == numberPressInLevel)
+            {
+                door.SetActive(false);
+                triggerRaycast.SetActive(true);
+            }
+        }
+    }
+
+
+}
