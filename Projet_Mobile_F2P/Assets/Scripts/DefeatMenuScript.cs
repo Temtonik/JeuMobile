6937e078159bc7b0d2799428800c9a68dec04629
TomRivaud <temtonik@gmail.com>
2018-10-15 14:28:21 +0200

    Scripts rangés + DefeatMenu sur UIManageV2.0

---
+++ Projet_Mobile_F2P/Assets/Scripts/DefeatMenuScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DefeatMenuScript : MonoBehaviour {
+
+    public string quitScene;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene(quitScene);
+    }
+}
