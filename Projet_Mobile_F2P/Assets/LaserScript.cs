480173e6c84d0f3626eb899c173fe6fa601a5eea
TomRivaud <temtonik@gmail.com>
2018-10-15 14:06:21 +0200

    Recup robot + piege + laser

---
+++ Projet_Mobile_F2P/Assets/LaserScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserScript : MonoBehaviour {
+
+    public GameObject laserAnim;
+    public GameObject canvasDefeat;
+    public int actionBeforeLaserActive = 1;
+
+	// Use this for initialization
+	void Start () {
+
+        //Au lancement de la scène la valeur est à 1
+        actionBeforeLaserActive = 1;
+    }
+
+// Update is called once per frame
+void Update () {
+
+        //Si valeur=0 le laser s'active sinon il se désactive
+		if (actionBeforeLaserActive == 0)
+        {
+            laserAnim.SetActive(true);
+        }
+        else
+        {
+            laserAnim.SetActive(false);
+        }
+
+        //Si la valeur est inférieur à -1 la mettre à 1
+        if (actionBeforeLaserActive == -1)
+        {
+            actionBeforeLaserActive = 1;
+        }
+	}
+
+    //En contact avec le laser le menu défaite s'affiche
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            canvasDefeat.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+}
---
+++ Projet_Mobile_F2P/Assets/PiegeActivation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiegeActivation : MonoBehaviour {
+
+    public GameObject electricTrapAnim;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player") 
+        {
+            //stamina -3;
+            electricTrapAnim.SetActive(true);
+            
+        }
+    }
+}
