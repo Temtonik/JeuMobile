112f4c6638dfe42f96f026a081c441ef32239ea1
TomRivaud <temtonik@gmail.com>
2018-10-16 15:10:10 +0200

    Boutique

---
+++ Projet_Mobile_F2P/Assets/Scripts/Boutique.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boutique : MonoBehaviour {
+
+    public GameObject boutiqueBonusCanvas;
+    public GameObject boutiqueEurosCanvas;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void boutiqueEurosButton()
+    {
+        boutiqueBonusCanvas.SetActive(false);
+        boutiqueEurosCanvas.SetActive(true);
+    }
+
+    public void boutiqueBonusButton()
+    {
+        boutiqueEurosCanvas.SetActive(false);
+        boutiqueBonusCanvas.SetActive(true);
+    }
+}
