1054b6b3d975001f995a182664e2ce97ca117577
WarZicks <36964591+WarZicks@users.noreply.github.com>
2018-10-22 09:24:46 +0200

    return_button_parrainage + add_img_victory_defeat

---
+++ Projet_Mobile_F2P/Assets/Scripts/UI/ParrainageManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ParrainageManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClickRetrun()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
