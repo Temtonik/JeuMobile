5eab6d5be0477a38a25ed86415e7401cbca5a35c
WarZicks <36964591+WarZicks@users.noreply.github.com>
2018-10-23 09:44:10 +0200

    update_contour_stars

--- Projet_Mobile_F2P/Assets/Scripts/Tuto.cs
+++ Projet_Mobile_F2P/Assets/Scripts/Tuto.cs
@@ -36,7 +36,7 @@ public class Tuto : MonoBehaviour {
     IEnumerator ShowTutoSwipeUp()
     {
 
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(3f);
         TutoGyro.SetActive(false);
         TutoSwipeUp.SetActive(true);
         StartCoroutine(HideTutoSwipeUp());
