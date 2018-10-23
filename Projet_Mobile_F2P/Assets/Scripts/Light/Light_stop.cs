d55e2b8ad7015cf75d4115c27cfac7e3e10b444b
Zookan <36925604+Zookan@users.noreply.github.com>
2018-10-23 11:28:30 +0200

    Light Level 3

---
+++ Projet_Mobile_F2P/Assets/Scripts/Light/Light_stop.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Light_stop : MonoBehaviour {
+
+    public GameObject StopLightInScene;
+    public GameObject ActiveGyroLight;
+    public GameObject ActiveLightRobot;
+
+    private bool lightActive;
+    private SwapMove my_Swipe;
+
+	// Use this for initialization
+	void Start ()
+    {
+        lightActive=true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GestionLight();
+	}
+
+    public void GestionLight()
+    {
+
+        if (my_Swipe.is_Moving && lightActive)
+        {
+            StopLightInScene.SetActive(false);
+            ActiveGyroLight.SetActive(true);
+            StartCoroutine(ActiveLightForRobot());
+        }
+
+    }
+
+    IEnumerator ActiveLightForRobot()
+    {
+        yield return new WaitForSeconds(0.5f);
+        ActiveLightRobot.SetActive(true);
+        
+    }
+}
