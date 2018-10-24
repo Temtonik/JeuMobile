a5e7498926a516624b354a912bfd6f9ac379497d
TomRivaud <temtonik@gmail.com>
2018-10-24 11:58:46 +0200

    Boutique avec feedback (pas dans les levels)

---
+++ Projet_Mobile_F2P/Assets/TextChanger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextChanger : MonoBehaviour {
+
+    public Text actionsBoughtText;
+    public Text starNumberText;
+    public Text lifeNumberText;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        actionsBoughtText.text = "" + ShopManager.Singleton.actionBought;
+        starNumberText.text = "" + ShopManager.Singleton.nbStar;
+        lifeNumberText.text = "" + ShopManager.Singleton.lifeBought;
+    }
+}
