2b83c223d69d8d25e42074063853eeddfecdaeb0
Stanislass <stanpython102s@hotmail.fr>
2018-10-25 11:01:52 +0200

    add_ui_boutique + sound_play

---
+++ Projet_Mobile_F2P/Assets/Scripts/UI/ConnexionManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnexionManager : MonoBehaviour {
+
+    public GameObject StarImg;
+    public GameObject Button;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClickButtonConnexion()
+    {
+        Destroy(Button);
+        StarImg.SetActive(true);
+        ShopManager.Singleton.nbStar++;
+    }
+}
---
+++ Projet_Mobile_F2P/Assets/Scripts/UI/PremiumManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PremiumManager : MonoBehaviour {
+
+    public static PremiumManager Singleton;
+
+    public GameObject Button;
+    public GameObject PremiumImg;
+    bool premiumMod;
+    GameObject Pub;
+
+    private void Awake()
+    {
+        if (Singleton != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Singleton = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        Pub = GameObject.FindGameObjectWithTag("Pub");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (premiumMod)
+        {
+            Pub.SetActive(false);
+        }
+	}
+
+    public void OnClickPremiumButton()
+    {
+        Destroy(Button);
+        PremiumImg.SetActive(true);
+        premiumMod = true;
+    }
+}
